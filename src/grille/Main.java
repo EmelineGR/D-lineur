51d9902e80c437178fd67ec1b3b74f513441edc0
alexhugon <43140888+alexhugon@users.noreply.github.com>
2018-10-30 13:27:05 +0100

    Update src/grille/Main.java

--- src/grille/Main.java
+++ src/grille/Main.java
@@ -7,8 +7,14 @@ public class Main
 		Case a = new Case(2,3);
 		a.setValeur(2);
 		System.out.println(a.getValeur());
-	
-	
-
+		Case[] grille = new Case[5*5];
+		for (int i = 0; i < grille.length; i++)
+		   {
+			grille[i] = new Case(1,1);
+		   }
+		System.out.println(grille[1].getValeur());
+		System.out.println(grille[2].getValeur());
+		System.out.println(grille[3].getValeur());
+		
 	}	
 }
