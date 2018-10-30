c9a089c4f4beaa65d0d19ff3638c2dfcc2937b2e
alexhugon <43140888+alexhugon@users.noreply.github.com>
2018-10-30 15:23:13 +0100

    Update src/grille/Main.java

--- src/grille/Main.java
+++ src/grille/Main.java
@@ -4,16 +4,12 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		Case a = new Case(2,3);
-		a.setValeur(6);
-		a.setImage(0);
-		//System.out.println(a.getValeur());
 		Case[][] grille = new Case[5][5];
 		for (int j = 0; j < grille.length; j++)
 		{
 			for (int i = 0; i < grille.length; i++)
 		  	{
-				grille[i][j] = new Case(1,1);
+				grille[i][j] = new Case(0,0);
 		  	}			
 		}
 
@@ -25,6 +21,7 @@ public class Main
 			}
 			System.out.println();
 		}
+		/* Debut du jeu*/
 		
 	}	
 }
